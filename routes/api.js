fe9e390b2596c1268ec8c5544d012ddd1e8ef56b
Drew Sessler <drew.sessler@gmail.com>
2022-03-23 01:13:01 -0700

    latest updates for api
    Co-authored-by: Michael Fatigati <fatigati.michael@gmail.com>
    Co-authored-by: Samantha Lipari <samanthalipari@gmail.com>
    Co-authored-by: Brendan Leal <leal.brendan@gmail.com>

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const Ansible = require("node-ansible");
-
-// const path = require("path");
+const appsController = require('../controllers/apps');
 
 // Get list of all running apps
-router.get('/apps', (req, res, next) => {
-  let playbook = new Ansible.Playbook().playbook('ansible/get_apps');
-  playbook.inventory('inventory/hosts');
-  let promise = playbook.exec();
-  promise.then((successResult) => {
-    console.log(successResult.code); // Exit code of the executed command
-    console.log("output: ", successResult.output); // Standard output/error of the executed command
-  }).catch((error) => {
-    console.error(error);
-  });
-});
+router.get('/apps', appsController.list);
+
+// Deploy a new app
+router.post('/apps', appsController.deploy);
+
+
+  // let playbook = new Ansible.Playbook().playbook('ansible/get_apps');
+  // playbook.inventory('inventory/hosts');
+  // let arr = [];
+  // playbook.on('stdout', function(data) {
+  //   arr.push(data.toString());
+  // });
+  // let promise = playbook.exec();
+  // promise.then((successResult) => {
+  //   console.log(successResult);
+  //   console.log(successResult.code); // Exit code of the executed command
+  //   console.log(successResult.output); // Standard output/error of the executed command
+  //   console.log(arr);
+  // }).catch((error) => {
+  //   console.error(error);
+  // });
 
 module.exports = router;
